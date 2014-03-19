17467ea68b7c7f0ecb603a6f2dd65fdcf7254235
Chris Price <chris@puppetlabs.com>
2014-03-19 10:52:39 -0700

    Add support for parsing config from readers
    Prior to this commit, the API only provided a way to parse
    config from files.  This commit adds the new `reader->map`
    function, which makes it possible to parse config
    from an InputStream or other reader-friendly object,
    rather than just from a file.

--- test/puppetlabs/config/typesafe_test.clj
+++ test/puppetlabs/config/typesafe_test.clj
@@ -1,4 +1,5 @@
 (ns puppetlabs.config.typesafe_test
+  (:import (java.io FileInputStream))
   (:require [puppetlabs.config.typesafe :as ts]
             [clojure.test :refer :all]))
 
@@ -28,3 +29,35 @@
                     :bap {:boozle "boozleboozle"
                           :bip [1 2 {:hi "there"} 3]}}}
              cfg)))))
+
+(deftest reader->map-test
+  (testing "can parse .properties stream with nested data structures"
+    (let [cfg (ts/reader->map
+                (FileInputStream. (str test-files-dir "config.properties"))
+                :properties)]
+      (is (= {:foo {:bar "barbar"
+                    :baz "bazbaz"
+                    :bam 42
+                    :bap {:boozle "boozleboozle"}}}
+             cfg))))
+  (testing "can parse .json stream with nested data structures"
+    (let [cfg (ts/reader->map
+                (FileInputStream. (str test-files-dir "config.json"))
+                :json)]
+      (is (= {:foo {:bar "barbar"
+                    :baz "bazbaz"
+                    :bam 42
+                    :bap {:boozle "boozleboozle"
+                          :bip [1 2 {:hi "there"} 3]}}}
+             cfg))))
+  (testing "can parse .conf stream with nested data structures"
+    (let [cfg (ts/reader->map
+                (FileInputStream. (str test-files-dir "config.conf"))
+                :conf)]
+      (is (= {:foo {:bar "barbar"
+                    :baz "bazbaz"
+                    :bam 42
+                    :bap {:boozle "boozleboozle"
+                          :bip [1 2 {:hi "there"} 3]}}}
+             cfg)))))
+

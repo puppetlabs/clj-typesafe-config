6a2dc6ff37098db190229ffd5508ec7a834303f7
Jenkins CI <ci@puppet.com>
2021-01-21 15:42:54 +0000

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/typesafe-config "0.2.0"
+(defproject puppetlabs/typesafe-config "0.2.1-SNAPSHOT"
   :description "Thin Clojure wrapper around Typesafe Config library"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
